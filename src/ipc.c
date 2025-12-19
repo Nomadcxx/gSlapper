2f59fa072f31f63a95fc49dfc95f28bb14118b9e
Nomadcxx <noovie@gmail.com>
2025-12-20 03:12:47 +1100

    Add socket creation function

--- src/ipc.c
+++ src/ipc.c
@@ -34,6 +34,38 @@ static pthread_mutex_t ipc_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 static void *ipc_server_thread_fn(void *arg);
 static void *ipc_client_thread_fn(void *arg);
 
+static int create_socket(const char *path) {
+    int sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock_fd < 0) {
+        cflp_error("Failed to create IPC socket: %s", strerror(errno));
+        return -1;
+    }
+
+    // Remove stale socket file
+    unlink(path);
+
+    struct sockaddr_un addr = {0};
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path) - 1);
+
+    if (bind(sock_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        cflp_error("Failed to bind IPC socket to %s: %s", path, strerror(errno));
+        close(sock_fd);
+        return -1;
+    }
+
+    if (listen(sock_fd, 5) < 0) {
+        cflp_error("Failed to listen on IPC socket: %s", strerror(errno));
+        close(sock_fd);
+        return -1;
+    }
+
+    // Set close-on-exec
+    fcntl(sock_fd, F_SETFD, FD_CLOEXEC);
+
+    return sock_fd;
+}
+
 // Stub implementations (will be replaced in later tasks)
 bool ipc_init(const char *path) {
     (void)path;
